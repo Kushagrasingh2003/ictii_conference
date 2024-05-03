d79e18b28d730284cb76fb3030562c5355d70f5b
Aditya <101622021+Kheraditya@users.noreply.github.com>
2024-05-03 17:01:06 +0530

    changes

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Register.css";
 import emailjs from "@emailjs/browser";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   //   const form = useRef();
+  useEffect(() => emailjs.init("pw-0MCe0N5hGr1c1y"), []);
   const [file1, setFile1] = useState(null);
   const [file2, setFile2] = useState(null);
+  const navigate = useNavigate();
 
   const [toSend, setToSend] = useState({
     From_name: "",
@@ -23,6 +26,7 @@ const Register = () => {
     modeOfPresentation: "",
     feeCategory: "",
     amountPaid: "",
+    attendanceDays: "",
     comments: "",
   });
 
@@ -33,38 +37,64 @@ const Register = () => {
     }
     const params = {
       from_name: toSend.From_name,
-      to_name: "DCE",
-      Designation: toSend.designation,
-      Email: toSend.email,
-      Mobile: toSend.mobile,
-      Institution: toSend.institution,
-      Address: toSend.address,
-      City: toSend.city,
-      State: toSend.state,
-      Country: toSend.country,
-      TitleOfResearch: toSend.titleOfResearch,
-      PaperId: toSend.paperId,
-      ModeOfPresentation: toSend.modeOfPresentation,
-      FeeCategory: toSend.feeCategory,
-      AmountPaid: toSend.amountPaid,
+      to_name: "Aditya",
+      designation: toSend.designation,
+      email: toSend.email,
+      mobile: toSend.mobile,
+      institution: toSend.institution,
+      address: toSend.address,
+      city: toSend.city,
+      state: toSend.state,
+      country: toSend.country,
+      researchTitle: toSend.titleOfResearch,
+      paperId: toSend.paperId,
+      modeOfPresentation: toSend.modeOfPresentation,
+      feeCategory: toSend.feeCategory,
+      amountPaid: toSend.amountPaid,
       PaidDetails: file1,
       ProofOfRegistration: file2,
-      attendanceDays: [],
-      Comments: toSend.comments,
+      attendanceDays: toSend.attendanceDays,
+      comments: toSend.comments,
     };
 
     emailjs
-      .send("service_1909vx8", "template_7dc632b", params, {
-        publicKey: "pw-0MCe0N5hGr1c1y",
-      })
+      .send(
+        "service_1909vx8",
+        "template_7dc632b",
+
+        params
+        // "pw-0MCe0N5hGr1c1y"
+      )
       .then(
         () => {
           console.log("SUCCESS!");
+          navigate("/");
+          console.log(params);
         },
         (error) => {
           console.log("FAILED...", error.text);
         }
       );
+
+    // setToSend({
+    //   From_name: "",
+    //   To_name: "DCE",
+    //   designation: "",
+    //   email: "",
+    //   mobile: "",
+    //   institution: "",
+    //   address: "",
+    //   city: "",
+    //   state: "",
+    //   country: "",
+    //   titleOfResearch: "",
+    //   paperId: "",
+    //   modeOfPresentation: "",
+    //   feeCategory: "",
+    //   amountPaid: "",
+    //   attendanceDays: "",
+    //   comments: "",
+    // });
   };
   const handleFileChange1 = (e) => {
     setFile1(e.target.files[0]);
@@ -72,31 +102,17 @@ const Register = () => {
   const handleFileChange2 = (e) => {
     setFile2(e.target.files[0]);
   };
-  //   const handleAttendanceChange = (e) => {
-  //     const { name, checked } = e.target;
-  //     let updatedAttendanceDays = [...toSend.attendanceDays];
-  //     if (checked) {
-  //       updatedAttendanceDays.push(name);
-  //     } else {
-  //       updatedAttendanceDays = updatedAttendanceDays.filter(
-  //         (day) => day !== name
-  //       );
-  //     }
-  //     setToSend({
-  //       ...toSend,
-  //       attendanceDays: updatedAttendanceDays,
-  //     });
-  //   };
+
+  // const handleChange = (e) => {
+  //   setToSend({ ...toSend, [e.target.name]: e.target.value });
+  // };
   const handleChange = (e) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setToSend({
+      ...toSend,
+      [name]: value,
+    });
   };
-  //   const handleChange = (e) => {
-  //     const { name, value } = e.target;
-  //     setToSend({
-  //       ...toSend,
-  //       [name]: value,
-  //     });
-  //   };
   return (
     <>
       <div className="container">
@@ -104,138 +120,128 @@ const Register = () => {
           <h1 className="headding">Conference Registration Form</h1>
           <form className="form" onSubmit={onSubmit}>
             <div className="form-group">
-              {" "}
               <label>Name:</label>
               <input
                 type="text"
                 name="From_name"
                 required
                 placeholder="name"
-                value={toSend.From_name}
+                // value={toSend.From_name}
                 onChange={handleChange}
               />
             </div>
 
             <div className="form-group">
-              {" "}
               <label>Designation:</label>
               <input
                 type="text"
                 name="designation"
                 required
                 placeholder="Your Designation"
-                value={toSend.designation}
+                // value={toSend.designation}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Email:</label>
               <input
                 type="email"
                 name="email"
                 required
                 placeholder="Your email"
-                value={toSend.email}
+                // value={toSend.email}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Mobile:</label>
               <input
                 type="tel"
                 name="mobile"
                 required
                 placeholder="Mobile"
-                value={toSend.mobile}
+                // value={toSend.mobile}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
-              <label>Institution(University/College/Organization) :</label>
+              <label>Institution(University/College/Organization):</label>
               <input
                 type="text"
                 name="institution"
                 required
                 placeholder="Institution"
-                value={toSend.institution}
+                // value={toSend.mobile}
                 onChange={handleChange}
               />
             </div>
+
             <div className="form-group">
               <label>Address:</label>
               <textarea
                 name="address"
                 required
                 placeholder="Address"
-                value={toSend.address}
+                // value={toSend.address}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>City:</label>
               <input
                 type="text"
                 name="city"
                 required
                 placeholder="City"
-                value={toSend.city}
+                // value={toSend.city}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>State:</label>
               <input
                 type="text"
                 name="state"
                 required
                 placeholder="State"
-                value={toSend.state}
+                // value={toSend.state}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Country:</label>
               <input
                 type="text"
                 name="country"
                 required
                 placeholder="Country"
-                value={toSend.country}
+                // value={toSend.country}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Title of Research Paper/Talk :</label>
               <input
                 type="text"
                 name="titleOfResearch"
                 required
                 placeholder="Title"
-                value={toSend.titleOfResearch}
+                // value={toSend.titleOfResearch}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Paper Id:</label>
               <input
-                type="number"
+                type="text"
                 name="paperId"
                 required
                 placeholder="PaperId"
-                value={toSend.paperId}
+                // value={toSend.paperId}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>
                 Mode of Paper Presentation(Oral/Poster/Physical/Online) :
               </label>
@@ -252,7 +258,6 @@ const Register = () => {
               </select>
             </div>
             <div className="form-group">
-              {" "}
               <label>Registration Fee Category :</label>
               <select
                 name="feeCategory"
@@ -272,19 +277,17 @@ const Register = () => {
               </select>
             </div>
             <div className="form-group">
-              {" "}
               <label>Registration Fee Amount Paid :</label>
               <input
                 type="text"
                 name="amountPaid"
                 required
                 placeholder="Amount Paid"
-                value={toSend.amountPaid}
+                // value={toSend.amountPaid}
                 onChange={handleChange}
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Upload Fee Paid Details/Proofs :</label>
               <input
                 type="file"
@@ -297,7 +300,6 @@ const Register = () => {
               />
             </div>
             <div className="form-group">
-              {" "}
               <label>Attach Proof of Registration Category(ID Card) :</label>
               <input
                 type="file"
@@ -310,46 +312,26 @@ const Register = () => {
               />
             </div>
 
-            {/* <div className="form-group">
-              <label>What days will you attend the conference?</label>
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="Day 1"
-                    checked={toSend.attendanceDays.includes("Day 1")}
-                    onChange={handleAttendanceChange}
-                  />
-                  Day 1
-                </label>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="Day 2"
-                    checked={toSend.attendanceDays.includes("Day 2")}
-                    onChange={handleAttendanceChange}
-                  />
-                  Day 2
-                </label>
-                <label>
-                  <input
-                    type="checkbox"
-                    name="Both (Day 1 & Day 2)"
-                    checked={toSend.attendanceDays.includes(
-                      "Both (Day 1 & Day 2)"
-                    )}
-                    onChange={handleAttendanceChange}
-                  />
-                  Both (Day 1 & Day 2)
-                </label>
-              </div>
-            </div> */}
+            <div className="form-group">
+              <label>What days will you attend the conference? :</label>
+              <select
+                name="attendanceDays"
+                value={toSend.attendanceDays}
+                onChange={handleChange}
+                required
+              >
+                <option value="">choose</option>
+                <option value="Day1">Day 1</option>
+                <option value="Day2">Day 2</option>
+                <option value="Both Day 1 & 2">Both Day 1 & 2</option>
+              </select>
+            </div>
             <div className="form-group">
               <label>Comments / Suggestions(If any):</label>
               <textarea
                 name="comments"
                 placeholder="Your message"
-                value={toSend.comments}
+                // value={toSend.comments}
                 onChange={handleChange}
               />
             </div>
